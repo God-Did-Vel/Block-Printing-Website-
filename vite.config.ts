2637edbbeb01da48242f46f5843f4fcb801e2d42
God-Did-Vel <mfonisocletus124@gmail.com>
2025-10-28 00:44:01 +0100

    initial-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "Block-Printing-Website-",
+  base: './',
 })
